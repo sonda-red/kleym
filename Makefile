5644e50bee50fb3c4179d5ae37349d32d468755b
Kalin Daskalov <kalin.daskalov@proton.me>
2026-01-14 22:00:36 +0200

    feat: add InferenceTrustProfile API and controller
    - Introduced the InferenceTrustProfile API with v1alpha1 version.
    - Created necessary types and generated deepcopy functions.
    - Added CustomResourceDefinition (CRD) for InferenceTrustProfile.
    - Implemented RBAC roles for admin, editor, and viewer access.
    - Developed controller logic for reconciling InferenceTrustProfile resources.
    - Added unit tests for the controller functionality.
    - Included sample manifest for InferenceTrustProfile.

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 # Image URL to use all building/pushing image targets
 IMG ?= controller:latest
 
+# Disable VCS stamping
+export GOFLAGS := -buildvcs=false
+
 # Get the currently used golang install path (in GOPATH/bin, unless GOBIN is set)
 ifeq (,$(shell go env GOBIN))
 GOBIN=$(shell go env GOPATH)/bin
--- cmd/main.go
+++ cmd/main.go
@@ -34,6 +34,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	terencesondaredv1alpha1 "github.com/sonda-red/terence/api/v1alpha1"
+	"github.com/sonda-red/terence/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -45,6 +48,8 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(terencesondaredv1.AddToScheme(scheme))
+	utilruntime.Must(terencesondaredv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -174,6 +179,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err := (&controller.InferenceTrustProfileReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "InferenceTrustProfile")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
